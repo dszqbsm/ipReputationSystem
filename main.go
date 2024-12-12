a78f9c6f77998f0f1aef3c955c9d55e25e193abc
sjh <570209909@qq.com>
2024-12-12 22:05:10 +0800

    feat✨: 封装api查询，基于gin框架开启web服务提供ip信誉查询

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
+// 从AbuseIPDB数据库查询指定IP的信誉分数并返回
+func abuseipdbCheck(ip string) (float64, error) {
 	// 数据准备
 	url := "https://api.abuseipdb.com/api/v2/check"
-	queryParams := "ipAddress=118.25.6.39&maxAgeInDays=90"
+	queryParams := fmt.Sprintf("ipAddress=%s&maxAgeInDays=90", ip)
 	headers := map[string]string{
 		"Accept":       "application/json",
 		"Key":          "e721369ffba7384c0b6c98b1a46e0b29a5b236b7cdfc84983f8e6bf3fae03c9567c06ea0b80871e2",
@@ -21,7 +24,7 @@ func main() {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		fmt.Printf("Error creating request: %v\n", err)
-		return
+		return -1, err
 	}
 	req.URL.RawQuery = queryParams    // 添加参数
 	for key, value := range headers { // 添加请求头
@@ -33,7 +36,7 @@ func main() {
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Printf("Error sending request: %v\n", err)
-		return
+		return -1, err
 	}
 	defer resp.Body.Close()
 
@@ -41,21 +44,40 @@ func main() {
 	body, err := ioutil.ReadAll(resp.Body) // 读取响应
 	if err != nil {
 		fmt.Printf("Error reading response body: %v\n", err)
-		return
+		return -1, err
 	}
 	var decodeResponse map[string]interface{}
 	err = json.Unmarshal(body, &decodeResponse) // 反序列化JSON
 	if err != nil {
 		fmt.Printf("Error parsing JSON: %v\n", err)
-		return
+		return -1, err
 	}
 
-	// 打印响应
-	output, err := json.MarshalIndent(decodeResponse, "", "  ")
-	if err != nil {
-		fmt.Printf("Error formatting JSON: %v\n", err)
-		return
+	// 提取abuseConfidenceScore
+	data, ok := decodeResponse["data"].(map[string]interface{})
+	if !ok {
+		fmt.Println("Error: data field is not a map")
+		return -1, err
 	}
+	abuseConfidenceScore, ok := data["abuseConfidenceScore"].(float64)
+	if !ok {
+		fmt.Println("Error: abuseConfidenceScore field is not a float64")
+		return -1, err
+	}
+	return abuseConfidenceScore, nil
+}
 
-	fmt.Println(string(output))
+func main() {
+	r := gin.Default()
+	// 请求示例：http://127.0.0.1:8090/?ip=127.0.0.1
+	r.GET("/", func(c *gin.Context) {
+		ip := c.Query("ip")
+		score, err := abuseipdbCheck(ip)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"score": score})
+		}
+	})
+	r.Run(":8090")
 }
